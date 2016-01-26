5823d4f95447bee2007648ed2fa13a4219320d96
joshquick <j.quick@bham.ac.uk>
2016-01-26 14:35:10 +0000

    Split indexes

--- indexdata.py
+++ indexdata.py
@@ -18,7 +18,7 @@ file.seek(1, 0)
 #output all data
 fname_tiles = 'IndexMetricsOut_tiles.txt'
 with open(fname_tiles, 'w') as tiles:
-	print >>tiles, ','.join(['Tile', 'Swath', 'Tile', 'Bool1', 'Bool2', 'Index', 'Read count', 'Sample name', 'Project name'])
+	print >>tiles, ','.join(['Tile', 'Swath', 'Tile', 'Bool1', 'Bool2', 'Index1', 'Index2', 'Read count', 'Sample name', 'Project name'])
 	i = 0
 	while True:
 		lane = struct.unpack('B', file.read(1))[0] #Lane
@@ -27,13 +27,13 @@ with open(fname_tiles, 'w') as tiles:
 		bool1 = struct.unpack('?', file.read(1))[0] #?
 		bool2 = struct.unpack('?', file.read(1))[0] #?
 		indexlen = struct.unpack('H', file.read(2))[0]
-		index = struct.unpack('%ss' %(indexlen), file.read(indexlen))[0]
+		index1, index2 = struct.unpack('%ss' %(indexlen), file.read(indexlen))[0].split('-')
 		readcount = struct.unpack('i', file.read(4))[0]
 		samplelen = struct.unpack('H', file.read(2))[0]
 		sample = struct.unpack('%ss' %(samplelen), file.read(samplelen))[0]
 		projectlen = struct.unpack('H', file.read(2))[0]
 		project = struct.unpack('%ss' %(projectlen), file.read(projectlen))[0]
-		print >>tiles, '%i,%i,%i,%s,%s,%s,%i,%s,%s' %(lane, swath, tile, bool1, bool2, index, readcount, sample, project)
+		print >>tiles, '%i,%i,%i,%s,%s,%s,%s,%i,%s,%s' %(lane, swath, tile, bool1, bool2, index1, index2, readcount, sample, project)
 		i += (16 + indexlen + samplelen + projectlen)
 		if i == length:
 			break
